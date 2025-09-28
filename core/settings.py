0885944cf8684d2e8c75fa69b394326d91656617
Ahmeddaud11 <shahwah7788@gmail.com>
2025-09-28 15:32:45 +0300

    added token lifetime

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,4 +130,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),   # short-lived access token
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),     # refresh token valid for 7 days
+    'ROTATE_REFRESH_TOKENS': True,                  # refresh generates a new access token
+    'BLACKLIST_AFTER_ROTATION': True,               # invalidate used refresh tokens
 }
